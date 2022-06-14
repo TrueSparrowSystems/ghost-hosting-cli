a8ed72800cd4ddd7f39370e544c72c7b1293a4ce
kreloaded <kamalakarkiran07@gmail.com>
2022-06-14 12:30:54 +0530

    added lib for get input and driver logic

---
+++ src/driver.ts
@@ -0,0 +1,36 @@
+const shell = require('shelljs');
+const promptSync = require("prompt-sync")({ sigint: true });
+
+console.log(typeof promptSync);
+
+function run(): void {
+    _getUserInput();
+
+    _applyPlanChanges();
+}
+
+function _getUserInput(): void {
+    const GetInput = require('../lib/GetInput');
+    new GetInput({ promptSync: promptSync }).perform();
+}
+
+function _applyPlanChanges(): void {
+    shell.exec('cdktf diff');
+
+    console.log('Please review the diff output above for ghost-hosting-cli');
+    const approve = promptSync("Do you want to approve?(y/n) (Applies the changes outlined in the plan): ");
+
+    if (approve === 'y') {
+        if (shell.exec('cdktf deploy --auto-approve').code !== 0) {
+            shell.echo('Error: cdktf deploy failed');
+        }
+    } else if (approve === 'n') {
+        console.log('Declined!');
+    } else {
+        console.log(`Invalid input! Please choose 'y' or 'n'`);
+    }
+
+    shell.exit(1);
+}
+
+run();
