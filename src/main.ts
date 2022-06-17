95630591721460c0415a1179b8b2167d4843911d
kreloaded <kamalakarkiran07@gmail.com>
2022-06-17 16:20:06 +0530

    rds changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Construct } from "constructs";
 import { App, TerraformStack, TerraformOutput, Fn } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws";
 import { Vpc } from '../.gen/modules/vpc';
+import { Rds } from '../.gen/modules/rds';
+import { SecurityGroup } from '../.gen/modules/security-group';
 
 import { readInput } from "../lib/readInput";
 import {
@@ -10,8 +12,8 @@ import {
 } from '../lib/util';
 
 const cidrPrefix = "10.0.0.0/16";
-const vpcName = "plg-gh-vpc";
-const rdsName = "plg-gh-rds";
+const vpcName = "PLG Ghost VPC";
+const rdsName = "plg-ghost-rds";
 
 class MyStack extends TerraformStack {
     userInput: any;
@@ -30,9 +32,40 @@ class MyStack extends TerraformStack {
             secretKey: this.userInput.aws.awsSecretAccessKey
         });
 
+
+        // VPC
+        const vpcOptions = this._getVpcOptions();
+        const vpc = new Vpc(this, vpcName, vpcOptions);
+
+        // RDS
+        new Rds(this, rdsName, {
+            identifier: rdsName,
+            engine: "mysql",
+            engineVersion: "5.7",
+            allocatedStorage: "10",
+            name: "testdb",
+            username: "username",
+            password: "password",
+            availabilityZone: "us-east-1a",
+            instanceClass: "db.t3.micro",
+            subnetIds: Fn.tolist(vpc.privateSubnetsOutput),
+            createDbSubnetGroup: true,
+            majorEngineVersion: "5.7",
+            parameterGroupName: "parameter-group-test-terraform",
+            parameterGroupDescription: "Parameter group for plg cdk",
+            family: "mysql5.7",
+            optionGroupName: "option-group-test-terraform",
+            dbSubnetGroupName: "db-group-test-terraform",
+            dbSubnetGroupUseNamePrefix: false,
+            parameterGroupUseNamePrefix: false,
+            optionGroupUseNamePrefix: false
+        });
+    }
+
+    _getVpcOptions() {
         const privateSubnetCidrBlocks = getPrivateSubnetCidrBlocks(cidrPrefix, 2, 2);
 
-        const vpc = new Vpc(this, vpcName, {
+        return {
             name: vpcName,
             azs: ["us-east-1a", "us-east-1b"],
             cidr: cidrPrefix,
@@ -47,7 +80,7 @@ class MyStack extends TerraformStack {
             enableNatGateway: true,
             singleNatGateway: true,
             enableDnsHostnames: true
-        });
+        }
     }
 }
 
